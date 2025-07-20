212d43bae6151baab592a27428fd176ead8973f8
GitHub Action <action@github.com>
2025-07-20 03:07:07 +0000

    Update packages: gemini-cli
    - gemini-cli: 0.1.12 → 0.1.13
    
    Auto-updated by update-packages workflow

--- packages/gemini-cli.nix
+++ packages/gemini-cli.nix
@@ -8,13 +8,13 @@
 
 gemini-cli.overrideAttrs (
   finalAttrs: prevAttrs: {
-    version = "0.1.12";
+    version = "0.1.13";
 
     src = fetchFromGitHub {
       owner = "google-gemini";
       repo = "gemini-cli";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-7StuYqKGnTTZY3BKK3X1kWNReRUfyvhfH3wGw0Pz2zM=";
+      hash = "sha256-50NMEPQ4sL0QWRkpZ7KYx+FM5luJ2w8iOINK85M4wN0=";
       postFetch = ''
         ${lib.getExe npm-lockfile-fix} $out/package-lock.json
       '';
@@ -22,7 +22,7 @@ gemini-cli.overrideAttrs (
 
     npmDeps = fetchNpmDeps {
       inherit (finalAttrs) src;
-      hash = "sha256-yt1Z/atE07vt27OdiLHPV1ZSHJ80zkGkcuro7rJxOrc=";
+      hash = "sha256-2GrF1HfsxWjQ5UY5HFvVWVpA/e8GkCBf+B4guLQjW6o=";
     };
   }
 )
