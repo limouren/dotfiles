99ffcbeffa6b406033a64800c675b1fdcb72c7d7
Kenji Pa <limouren@gmail.com>
2025-07-24 00:47:57 +0800

    Fix gemini-cli with proper vscode extension built

--- packages/gemini-cli.nix
+++ packages/gemini-cli.nix
@@ -14,15 +14,25 @@ gemini-cli.overrideAttrs (
       owner = "google-gemini";
       repo = "gemini-cli";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-50NMEPQ4sL0QWRkpZ7KYx+FM5luJ2w8iOINK85M4wN0=";
-      postFetch = ''
-        ${lib.getExe npm-lockfile-fix} $out/package-lock.json
-      '';
+      hash = "sha256-egQlKqS5pD1mIZChmT2LLnrDq8U+5RbvLNDNhkd5vhE=";
     };
 
+    patches = [ ./gemini-cli.patch ];
+
     npmDeps = fetchNpmDeps {
-      inherit (finalAttrs) src;
-      hash = "sha256-2GrF1HfsxWjQ5UY5HFvVWVpA/e8GkCBf+B4guLQjW6o=";
+      inherit (finalAttrs) src patches;
+      hash = "sha256-lCyTT9TNDvBRueNlU4uR3gLWv2qoICq6xFMLdhFPBQI=";
     };
+
+    postInstall =
+      (prevAttrs.postInstall or "")
+      + ''
+        # Remove broken symlink to VSCode extension
+        rm -f $out/share/gemini-cli/node_modules/gemini-cli-vscode-ide-companion
+
+        # Copy VSCode extension to main output
+        mkdir -p $out/share/vscode/extensions
+        cp packages/vscode-ide-companion/gemini-cli-vscode-ide-companion-${finalAttrs.version}.vsix $out/share/vscode/extensions/
+      '';
   }
 )
