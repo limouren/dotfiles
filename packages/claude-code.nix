be847f7d1ee58807b4d1982895488dbd0f03c466
Kenji Pa <limouren@gmail.com>
2025-12-20 02:26:29 +0800

    Update claude-code: 2.0.72 -> 2.0.73

--- packages/claude-code.nix
+++ packages/claude-code.nix
@@ -5,11 +5,11 @@
 
 claude-code.overrideAttrs (
   finalAttrs: prevAttrs: {
-    version = "2.0.72";
+    version = "2.0.73";
 
     src = fetchurl {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-      hash = "sha256-MeDnwe98890zSqRvqqePzrzYirKrLNmE2P2wL9lxaK0=";
+      hash = "sha256-dsGZNan4isDgm4j8zvdIjOHQn9OJj8NXqvKRXP96gTA=";
     };
   }
 )
